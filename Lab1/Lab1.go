cc56673bf65efefe895101b307185c4644281593
Thoth <kanansak555@gmail.com>
2023-03-23 15:47:25 +0700

    Update Lab1.go linux-2

--- Lab1/Lab1.go
+++ Lab1/Lab1.go
@@ -28,30 +28,30 @@ func initialized() {
 }
 
 func showProcess() {
-	fmt.Printf("\n-----------\n")
-	fmt.Printf("CPU1   -> %s\n", cpu1)
-	fmt.Printf("CPU2   -> %s\n", cpu2)
+	fmt.Printf("\n------------------\n")
+	fmt.Printf("CPU_1 -> %s\n", cpu1)
+	fmt.Printf("CPU_2 -> %s\n", cpu2)
 	fmt.Printf("Ready -> ")
 	for i := range ready {
 		fmt.Printf("%s ", ready[i])
 	}
 	fmt.Printf("\n")
-	fmt.Printf("I/O 1 -> ")
+	fmt.Printf("I/O_1 -> ")
 	for i := range io1 {
 		fmt.Printf("%s ", io1[i])
 	}
 	fmt.Printf("\n")
-	fmt.Printf("I/O 2 -> ")
+	fmt.Printf("I/O_2 -> ")
 	for i := range io2 {
 		fmt.Printf("%s ", io2[i])
 	}
 	fmt.Printf("\n")
-	fmt.Printf("I/O 3 -> ")
+	fmt.Printf("I/O_3 -> ")
 	for i := range io3 {
 		fmt.Printf("%s ", io3[i])
 	}
 	fmt.Printf("\n")
-	fmt.Printf("I/O 4 -> ")
+	fmt.Printf("I/O_4 -> ")
 	for i := range io4 {
 		fmt.Printf("%s ", io4[i])
 	}
@@ -74,18 +74,26 @@ func command_new(p string) {
 		insertQueue(ready, p)
 	}
 }
-func command_terminate_c1() {
-	if cpu1 != "" {
-		cpu1 = deleteQueue(ready)
+func command_terminate(namecpu string) {
+	if namecpu == "cpu1" {
+		if cpu1 != "" {
+			cpu1 = deleteQueue(ready)
+		}
 	}
-}
-func command_terminate_c2() {
-	if cpu2 != "" {
-		cpu2 = deleteQueue(ready)
+	if namecpu == "cpu2" {
+		if cpu2 != "" {
+			cpu2 = deleteQueue(ready)
+		}
 	}
 }
 
-func command_expire_c1() {
+/*func command_terminate_cpu2() {
+	if cpu2 != "" {
+		cpu2 = deleteQueue(ready)
+	}
+}*/
+
+func command_expire_cpu1() {
 	p := deleteQueue(ready)
 	if p == "" {
 		return
@@ -93,7 +101,7 @@ func command_expire_c1() {
 	insertQueue(ready, cpu1)
 	cpu1 = p
 }
-func command_expire_c2() {
+func command_expire_cpu2() {
 	p := deleteQueue(ready)
 	if p == "" {
 		return
@@ -101,45 +109,45 @@ func command_expire_c2() {
 	insertQueue(ready, cpu2)
 	cpu2 = p
 }
-func command_io1_c1() {
+func command_io1_cpu1() {
 	insertQueue(io1, cpu1)
 	cpu1 = ""
-	command_expire_c1()
+	command_expire_cpu1()
 }
-func command_io1_c2() {
+func command_io1_cpu2() {
 	insertQueue(io1, cpu2)
 	cpu2 = ""
-	command_expire_c2()
+	command_expire_cpu2()
 }
-func command_io2_c1() {
+func command_io2_cpu1() {
 	insertQueue(io2, cpu1)
 	cpu1 = ""
-	command_expire_c1()
+	command_expire_cpu1()
 }
-func command_io2_c2() {
+func command_io2_cpu2() {
 	insertQueue(io2, cpu2)
 	cpu2 = ""
-	command_expire_c2()
+	command_expire_cpu2()
 }
-func command_io3_c1() {
+func command_io3_cpu1() {
 	insertQueue(io3, cpu1)
 	cpu1 = ""
-	command_expire_c1()
+	command_expire_cpu1()
 }
-func command_io3_c2() {
+func command_io3_cpu2() {
 	insertQueue(io3, cpu2)
 	cpu2 = ""
-	command_expire_c2()
+	command_expire_cpu2()
 }
-func command_io4_c1() {
+func command_io4_cpu1() {
 	insertQueue(io4, cpu1)
 	cpu1 = ""
-	command_expire_c1()
+	command_expire_cpu1()
 }
-func command_io4_c2() {
+func command_io4_cpu2() {
 	insertQueue(io4, cpu2)
 	cpu2 = ""
-	command_expire_c2()
+	command_expire_cpu2()
 }
 func command_io1x() {
 	p := deleteQueue(io1)
@@ -232,30 +240,30 @@ func main() {
 				}
 				command_new(commandx[i])
 			}
-		case "terminate1":
-			command_terminate_c1()
-		case "terminate2":
-			command_terminate_c2()
+		case "terminate":
+			command_terminate(commandx[1])
+		//case "terminate2":
+		//	command_terminate_cpu2()
 		case "expire1":
-			command_expire_c1()
+			command_expire_cpu1()
 		case "expire2":
-			command_expire_c2()
-		case "io11":
-			command_io1_c1()
-		case "io12":
-			command_io1_c2()
-		case "io21":
-			command_io2_c1()
-		case "io22":
-			command_io2_c2()
-		case "io31":
-			command_io3_c1()
-		case "io32":
-			command_io3_c2()
-		case "io41":
-			command_io4_c1()
-		case "io42":
-			command_io4_c2()
+			command_expire_cpu2()
+		case "io1cpu1":
+			command_io1_cpu1()
+		case "io1cpu2":
+			command_io1_cpu2()
+		case "io2cpu1":
+			command_io2_cpu1()
+		case "io2cpu2":
+			command_io2_cpu2()
+		case "io3cpu1":
+			command_io3_cpu1()
+		case "io3cpu2":
+			command_io3_cpu2()
+		case "io4cpu1":
+			command_io4_cpu1()
+		case "io4cpu2":
+			command_io4_cpu2()
 		case "io1x":
 			command_io1x()
 		case "io2x":
