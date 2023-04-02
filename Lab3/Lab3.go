5fe4def24416efaae62f4701a066dd09b3a6c9ca
Thoth <kanansak555@gmail.com>
2023-04-02 09:16:53 +0700

    Update Lab3.go

--- Lab3/Lab3.go
+++ Lab3/Lab3.go
@@ -6,17 +6,14 @@ import (
 	"time"
 )
 
-var (
-	buffer = make(chan int)
-)
+var buffer = make(chan int)
 
 func producer(id int) {
 	for {
-
 		data := rand.Intn(20) + 1    // Generate a random integer between 1 and 20
 		duration := rand.Intn(4) + 1 // Generate a random integer between 1 and 4
 		buffer <- data               // Add the value to the buffer
-		fmt.Printf("Producer %d produced data: %d TimeSleep %d\n", id, data, duration)
+		fmt.Printf("Producer %d produced data: %d | Time Sleep: %d\n", id, data, duration)
 
 		// Sleep for a random duration between 1 and 4 seconds
 		time.Sleep(time.Duration(duration) * time.Second)
@@ -29,7 +26,7 @@ func consumer(id int) {
 		duration := rand.Intn(4) + 1 // Generate a random integer between 1 and 4
 		if ok {
 			fib := fib(data) // Calculate the Fibonacci
-			fmt.Printf("Consumer %d Fibonacci value for %d: %d TimeSleep %d\n", id, data, fib, duration)
+			fmt.Printf("Consumer %d | Fibonacci value for %d: %d | Time Sleep: %d\n", id, data, fib, duration)
 		} else {
 			fmt.Println("No data in buffer")
 		}
